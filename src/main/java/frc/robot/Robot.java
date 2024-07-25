aaf91f8d8ec741380657acc3676f39affee28751
Zevzzz <46076391+Zevzzz@users.noreply.github.com>
2024-07-25 11:32:47 -0700

    Disabled serial port

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,7 +19,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 public class Robot extends TimedRobot {
   
   private Command m_autonomousCommand;
-  SerialPort serialPort;
+  // SerialPort serialPort;
 
   private RobotContainer m_robotContainer;
 
@@ -32,7 +32,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    this.serialPort = new SerialPort(9600, SerialPort.Port.kUSB1);
+    // this.serialPort = new SerialPort(9600, SerialPort.Port.kUSB1);
   }
 
   /**
@@ -50,8 +50,8 @@ public class Robot extends TimedRobot {
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
 
-    SmartDashboard.putString("Serial Port Output", serialPort.readString());
-    System.out.println(serialPort.readString());
+    // SmartDashboard.putString("Serial Port Output", serialPort.readString());
+    // System.out.println(serialPort.readString());
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
